b28ef8aec80cd68fc261a9016f106b4b9143a06e
JessonChan <chinachendongpo@gmail.com>
2020-01-28 19:00:33 +0800

    添加主文件

--- examples/shorten_url/shorten.go
+++ examples/shorten_url/shorten.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/JessonChan/cango"
+	"github.com/JessonChan/cango/examples/shorten_url/controllers"
+)
 
+func main() {
+	cango.NewCan().
+		Route(&controllers.AdminController{}, &controllers.ShorterController{}).
+		Run(cango.Addr{Port: 8088})
 }
