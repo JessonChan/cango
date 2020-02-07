feea52f02d4b6bf57dd7570d94c06d738d632eeb
JessonChan <chinachendongpo@gmail.com>
2020-02-07 16:32:05 +0800

    session module

---
+++ session.go
@@ -0,0 +1,31 @@
+// Copyright 2020 Cango Author.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package cango
+
+import (
+	"net/http"
+)
+
+type sessionStore interface {
+	New(r *http.Request, name string)
+	Get(r *http.Request, name string) (*session, error)
+	Put(r *http.Request, w http.ResponseWriter, s *session) error
+}
+
+// Session stores the values  for a session.
+type session struct {
+	ID     string
+	Values map[interface{}]interface{}
+	name   string
+}
