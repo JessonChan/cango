bbeb9f44902f7dd2d416a14943a2b22b63312f13
JessonChan <chinachendongpo@gmail.com>
2020-02-28 23:55:54 +0800

    cookie 模块

--- cookie.go
+++ cookie.go
@@ -12,3 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package cango
+
+type Cookie interface {
+}
+
+type CanCookie struct {
+	Cookie
+}
