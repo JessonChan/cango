34144ac12d28864f457ac748ef1bba01ff724fff
JessonChan <chinachendongpo@gmail.com>
2020-03-02 23:16:10 +0800

    clean code

--- controller_static.go
+++ controller_static.go
@@ -17,15 +17,6 @@ type staticController struct {
 	URI
 }
 
-// var _ = RegisterURI(&staticController{})
-
-// func (s *staticController) Get(ps struct {
-// 	URI `value:"/static/*;/favicon.ico;/robots.txt"`
-// 	GetMethod
-// }) StaticFile {
-// 	return StaticFile{s.URI.Request().URL.Path}
-// }
-
-func (s *staticController) Get(URI) StaticFile {
-	return StaticFile{s.URI.Request().URL.Path}
+func (s *staticController) Get(uri URI) StaticFile {
+	return StaticFile{uri.Request().URL.Path}
 }
