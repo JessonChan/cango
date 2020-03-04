fcc0758aaede4eb7ed351c3d1b22342d5bec3abc
JessonChan <chinachendongpo@gmail.com>
2020-03-05 00:25:29 +0800

    取参数的struct

--- factory_test.go
+++ factory_test.go
@@ -6,6 +6,10 @@ import (
 )
 
 type Struct struct {
+	URI
+}
+
+type NotURI struct {
 }
 
 func Test_toPtrKind(t *testing.T) {
@@ -32,6 +36,51 @@ func Test_toPtrKind(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := toPtrKind(tt.args.i); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("toPtrKind() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_implements(t *testing.T) {
+	type args struct {
+		in  reflect.Type
+		typ reflect.Type
+	}
+	tests := []struct {
+		name string
+		args args
+		want reflect.Type
+	}{
+		{
+			name: "struct",
+			args: args{
+				in:  reflect.TypeOf(Struct{}),
+				typ: uriType,
+			},
+			want: reflect.TypeOf(Struct{}),
+		},
+		{
+			name: "ptr",
+			args: args{
+				in:  reflect.TypeOf(&Struct{}),
+				typ: uriType,
+			},
+			want: reflect.TypeOf(Struct{}),
+		},
+		{
+			name: "not_uri",
+			args: args{
+				in:  reflect.TypeOf(&NotURI{}),
+				typ: uriType,
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := implements(tt.args.in, tt.args.typ)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("implements() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
